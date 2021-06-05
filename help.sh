6cc2ca1e230cfe54c63e595d9a2af944658b6625
Gourav Kumar <67992259+Gourav-Kr@users.noreply.github.com>
2021-06-05 11:53:51 +0530

    Updated Author

--- help.sh
+++ help.sh
@@ -5,6 +5,8 @@ printf "\n\t\t\t  \033[4;1;103m \e[1;30m-----ITW FINAL PROJECT-----\033[0m\n\n\0
 2: In System Status Menu you will find commands like ps, df, date, printenv
 3: In Text Menu you will find commands like grep, diff, wc\n"
 
+echo -e "\n\e[1;95mAuthor : Gourav Kumar \e[0m"
+
 printf "\n\n \t\t\e[5;1;91mPress any key to go back to main program\e[0m\n"
 read opt
 printf "\033[1;32mOpening main menu\033[0m"
